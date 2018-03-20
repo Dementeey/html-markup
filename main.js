b8e1ed0dd58af402863ff87fb5009ae43ecb6373
Demeneteey ES <metallist9656@gmail.com>
2018-03-20 19:56:14 +0200

    second commit

---
+++ main.js
@@ -0,0 +1,35 @@
+const btn = document.querySelector(".btn");
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const containerBefore = document.querySelector(".container::before");
+
+
+const toggleBG = () => {
+  // if (left.classList.contains("bgc1")  left.classList.contains("bgc1" || "bgc2")  === true) {
+   
+  // containerBefore.style.background = 'red';
+  containerBefore.style.backgroundColor = "red";
+  // }
+  // return left.classList.contains("bgc1")  === true ?  left.classList.toggle("bgc1") : left.classList.toggle("bgc2");
+  
+  // left.classList.add("bgc2");
+  // right.classList.remove("bgc2");
+  // right.classList.add("bgc1");
+}
+
+
+
+
+
+
+// if (btn.tagName === bgc1) {
+//   classList.remove("sepiaBody");
+// }
+// if (this.checked) {
+//   darkBody.classList.add("darkBody");
+// } else {
+//   darkBody.classList.remove("darkBody");
+// };
+// btn.addEventListener("click", function(event) {
+
+// });
