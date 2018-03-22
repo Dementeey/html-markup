0f8114fec1ccd359438a52b1d8d5ec71e44fe7a0
Demeneteey ES <metallist9656@gmail.com>
2018-03-22 19:04:38 +0200

    save ES

--- main.js
+++ main.js
@@ -1,35 +1,27 @@
 const btn = document.querySelector(".btn");
-const left = document.querySelector(".left");
-const right = document.querySelector(".right");
-const containerBefore = document.querySelector(".container::before");
+// const left = document.querySelector(".left");
+// const right = document.querySelector(".right");
+const container = document.querySelector(".container");
 
 
 const toggleBG = () => {
-  // if (left.classList.contains("bgc1")  left.classList.contains("bgc1" || "bgc2")  === true) {
-   
-  // containerBefore.style.background = 'red';
-  containerBefore.style.backgroundColor = "red";
-  // }
-  // return left.classList.contains("bgc1")  === true ?  left.classList.toggle("bgc1") : left.classList.toggle("bgc2");
-  
-  // left.classList.add("bgc2");
-  // right.classList.remove("bgc2");
-  // right.classList.add("bgc1");
+  const newStyle = document.createElement('style');
+  newStyle.type = 'text/css';
+  newStyle.appendChild(document.createTextNode('\
+      .container::before {\
+          background-color: #666;\
+      }\
+      .container::after {\
+          background-color: #000;\
+      }\
+  '));
+  document.head.appendChild(newStyle);
 }
 
+const toggleBG__2 = () => {
+  if (container.classList.contains('Index') === true) {
+    container.classList.remove("Index")
+    container.classList.add("About");
+  } 
 
-
-
-
-
-// if (btn.tagName === bgc1) {
-//   classList.remove("sepiaBody");
-// }
-// if (this.checked) {
-//   darkBody.classList.add("darkBody");
-// } else {
-//   darkBody.classList.remove("darkBody");
-// };
-// btn.addEventListener("click", function(event) {
-
-// });
+}
